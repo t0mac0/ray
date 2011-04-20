03b93506b136d8d27c0a09a6d4cab3706e06bbfd
Francisco Mota <fmota91@gmail.com>
2011-04-20 18:37:16 -0500

    Refactored render2D

--- src/screen.c
+++ src/screen.c
@@ -2,12 +2,17 @@
 
 void Screen_render2D(Screen* self, Scene* scene) {
     Vec3 origin = Vec3_new(0, 0, -5.0f);
+    float sy =  0.005f * (float)self->height;
+    float sx = -0.005f * (float)self->width;
+    float dy = -0.01f;
+    float dx =  0.01f;
 
-    for (size_t y = 0; y < self->height; y++) {
-        float fy = -(0.01f*(float)y - 0.005f*(float)self->height);
+
+    float fy = sy;
+    for (size_t y = 0; y < self->height; y++, fy += dy) {
         void* pixelrow = ((char*)self->pixels) + y * self->pitch;
-        for (size_t x = 0; x < self->width; x++) {
-            float fx = 0.01f*(float)x - 0.005f*(float)self->width;
+        float fx = sx;
+        for (size_t x = 0; x < self->width; x++, fx += dx) {
             Vec3 dir = Vec3_normal(Vec3_new(fx, fy, 5.0f));
             ((Color*)pixelrow)[x] = Scene_cast(scene, origin, dir);
         }
